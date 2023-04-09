fb8073d5d3f2b3c033cbf728c6778aa28538400d
Alex <40631201+DrekaaR@users.noreply.github.com>
2023-04-09 21:56:51 +0200

    minor refactoring

--- src/components/TokensTabs/Assets/Assets.jsx
+++ src/components/TokensTabs/Assets/Assets.jsx
@@ -1,46 +1,81 @@
 import { ReactComponent as CrossIcon } from "assets/svg/cross.svg";
 import { ReactComponent as PlusIcon } from "assets/svg/plus.svg";
+import { ReactComponent as BitcoinIcon } from "assets/tokens-svg/bitcoin.svg";
+import { ReactComponent as CustomTokenIcon } from "assets/tokens-svg/custom-token.svg";
+import { ReactComponent as EthIcon } from "assets/tokens-svg/eth.svg";
+import { ReactComponent as ZanoIcon } from "assets/tokens-svg/zano.svg";
 import MyButton from "components/.UI/MyButton/MyButton";
-import React from 'react';
-import s from "./Assets.module.scss"
+import React from "react";
+import s from "./Assets.module.scss";
 
-const Assets = ({assetsMap}) => {
-   const remove = () => {
-      alert("remove")
-   }
+const assetsMap = [
+  {
+    name: "ZANO",
+    icon: <ZanoIcon />,
+    balance: 120,
+    ticker: "ZANO",
+    value: 128.96,
+  },
+  {
+    name: "Wrapped Bitcoin",
+    icon: <BitcoinIcon />,
+    balance: 0.212,
+    ticker: "WBTC",
+    value: 4096.96,
+  },
+  {
+    name: "Wrapped Ethereum",
+    icon: <EthIcon />,
+    balance: 2.1,
+    ticker: "WETH",
+    value: 3020.12,
+  },
+  {
+    name: "Custom Asset",
+    icon: <CustomTokenIcon />,
+    balance: 15.52,
+    ticker: "TSDS",
+    value: 3020.12,
+  },
+];
 
-   return (
-      <div>
-         {assetsMap.map(asset =>
-            <div className={s.asset}>
-               <button className={s.assetRemoveBtn} onClick={remove}>
-                  <CrossIcon/>
-               </button>
-               <button className={s.assetBody}>
-                  <span className={s.assetTitle}>
-                     {asset.icon}
-                     {asset.name}
-                  </span>
-                  <span className={s.assetInfo}>
-                     <div>
-                        <div className={s.assetInfoLabel}>Balance</div>
-                        <div className={s.assetInfoValue}>
-                           {[asset.balance, asset.ticker].join(" ")}
-                        </div>
-                     </div>
-                     <div>
-                        <div className={s.assetInfoLabel}>Value</div>
-                        <div className={s.assetInfoValue}>${asset.value}</div>
-                     </div>
-                  </span>
-               </button>
-            </div>
-         )}
-         <MyButton style={{transform: "translateY(30%)"}}>
-            <PlusIcon/> Add Custom Token
-         </MyButton>
-      </div>
-   )
+const Assets = () => {
+  const remove = () => {
+    alert("remove");
+  };
+
+  return (
+    <div>
+      {assetsMap.map((asset) => (
+        <div className={s.asset}>
+          <button className={s.assetRemoveBtn} onClick={remove}>
+            <CrossIcon />
+          </button>
+          <button className={s.assetBody}>
+            <span className={s.assetTitle}>
+              {asset.icon}
+              {asset.name}
+            </span>
+            <span className={s.assetInfo}>
+              <div>
+                <div className={s.assetInfoLabel}>Balance</div>
+                <div className={s.assetInfoValue}>
+                  {[asset.balance, asset.ticker].join(" ")}
+                </div>
+              </div>
+              <div>
+                <div className={s.assetInfoLabel}>Value</div>
+                <div className={s.assetInfoValue}>${asset.value}</div>
+              </div>
+            </span>
+          </button>
+        </div>
+      ))}
+      <MyButton style={{ transform: "translateY(30%)" }}>
+        <PlusIcon /> Add Custom Token
+      </MyButton>
+    </div>
+  );
 };
 
 export default Assets;
--- src/components/TokensTabs/TokensTabs.jsx
+++ src/components/TokensTabs/TokensTabs.jsx
@@ -1,93 +1,41 @@
-import { ReactComponent as BitcoinIcon } from "assets/tokens-svg/bitcoin.svg";
-import { ReactComponent as CustomTokenIcon } from "assets/tokens-svg/custom-token.svg";
-import { ReactComponent as EthIcon } from "assets/tokens-svg/eth.svg";
-import { ReactComponent as ZanoIcon } from "assets/tokens-svg/zano.svg";
 import Assets from "components/TokensTabs/Assets/Assets";
 import History from "components/TokensTabs/History/History";
 import s from "components/TokensTabs/TokensTabs.module.scss";
 import React, { useState } from "react";
 
-const assetsMap = [
-   {
-      name: "ZANO",
-      icon: <ZanoIcon/>,
-      balance: 120,
-      ticker: "ZANO",
-      value: 128.96,
-   },
-   {
-      name: "Wrapped Bitcoin",
-      icon: <BitcoinIcon/>,
-      balance: 0.212,
-      ticker: "WBTC",
-      value: 4096.96,
-   },
-   {
-      name: "Wrapped Ethereum",
-      icon: <EthIcon/>,
-      balance: 2.1,
-      ticker: "WETH",
-      value: 3020.12,
-   },
-   {
-      name: "Custom Asset",
-      icon: <CustomTokenIcon/>,
-      balance: 15.52,
-      ticker: "TSDS",
-      value: 3020.12,
-   },
-];
-
-const historyMap = [
-   {
-      value: 112412,
-      type: "send", // or "receive"
-      ticker: "ZANO",
-      address: "ZxDCjtvEPnwKFPa9Hy5frFbQoT6KQaR7gbog7oT6KQaR7gbog7",
-      inProcess: false,
-   },
-   {
-      value: 10,
-      type: "receive", // or "send"
-      ticker: "ZANO",
-      address: "ZxDCjtvEPnwKFPa9Hy5frFbQoT6KQaR7gbog7oT6KQaR7gbog7",
-      inProcess: true,
-   }
-];
-
 const TokensTabs = () => {
-   const [activeTab, setActiveTab] = useState(1);
+  const [activeTab, setActiveTab] = useState(1);
 
-   const tabs = [
-      {label: "assets", content: <Assets assetsMap={assetsMap}/>},
-      {label: "history", content: <History historyMap={historyMap}/>},
-   ];
+  const tabs = [
+    { label: "assets", content: <Assets /> },
+    { label: "history", content: <History /> },
+  ];
 
-   const toggleTabs = (e) => {
-      if (activeTab !== e.target.value) {
-         setActiveTab(Number(e.target.value));
-      }
-   };
+  const toggleTabs = (e) => {
+    if (activeTab !== e.target.value) {
+      setActiveTab(Number(e.target.value));
+    }
+  };
 
-   return (
-      <div className={s.tabs}>
-         <div className={s.tabsNav}>
-            {tabs.map((tab, index) => (
-               <button
-                  key={tab.label}
-                  onClick={(e) => toggleTabs(e)}
-                  value={index}
-                  disabled={activeTab === index}
-                  className={s.tabsNavBtn}
-               >
-                  {tab.label}
-               </button>
-            ))}
-         </div>
-
-         {tabs[activeTab].content}
+  return (
+    <div className={s.tabs}>
+      <div className={s.tabsNav}>
+        {tabs.map((tab, index) => (
+          <button
+            key={tab.label}
+            onClick={(e) => toggleTabs(e)}
+            value={index}
+            disabled={activeTab === index}
+            className={s.tabsNavBtn}
+          >
+            {tab.label}
+          </button>
+        ))}
       </div>
-   );
+
+      {tabs[activeTab].content}
+    </div>
+  );
 };
 
 export default TokensTabs;
