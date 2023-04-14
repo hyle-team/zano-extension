f7c038030edf58018b3b090e5f9f6eed81ca7387
Alex <40631201+DrekaaR@users.noreply.github.com>
2023-04-14 15:53:18 +0200

    app plug added

--- src/app/components/AppPlug/AppPlug.jsx
+++ src/app/components/AppPlug/AppPlug.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import logo from "../../assets/svg/logo.svg"
-import s from "./AppPlug.module.scss"
+import React from "react";
+import displayImage from "../../assets/images/display.svg";
+import logo from "../../assets/svg/logo.svg";
+import questionIcon from "../../assets/svg/question.svg";
+import s from "./AppPlug.module.scss";
 
 const AppPlug = () => {
   return (
-    <div className="container">
-      <div className={s.plugLogo}>
-        <img src={logo} alt="zano logo"/>
+    <div className={s.plug}>
+      <div className={`${s.plugBody} container`}>
+        <div className={s.plugLogo}>
+          <img src={logo} alt="zano logo" />
+        </div>
+
+        <div className={s.plugContent}>
+          <div className={s.plugImage}>
+            <img src={displayImage} alt="display image" />
+          </div>
+          <span>We can’t find a Zano wallet app on your device</span>
+        </div>
+
+        <button className={s.plugButton}>
+          <img src={questionIcon} alt="question icon" />
+          How to create wallet?
+        </button>
       </div>
     </div>
   );
