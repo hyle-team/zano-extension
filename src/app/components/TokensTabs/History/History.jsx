a2165d8cddd347fdcbb6a2b162e736551ac857f3
PRavaga <trueravaga@gmail.com>
2023-06-28 20:38:52 +0200

    incoming amount fix
    Signed-off-by: PRavaga <trueravaga@gmail.com>

--- src/app/components/TokensTabs/History/History.jsx
+++ src/app/components/TokensTabs/History/History.jsx
@@ -40,7 +40,9 @@ const History = () => {
                   <span>
                     {transfer.assetId ===
                     "d6329b5b1f7c0805b5c345f4957554002a2f557845f64d7645dae0e051a6498a"
-                      ? transfer.amount - tx.fee
+                      ? transfer.incoming
+                        ? transfer.amount
+                        : transfer.amount - tx.fee
                       : transfer.amount}{" "}
                     {
                       whitelistedAssets.find(
